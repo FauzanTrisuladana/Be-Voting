6ff25e845a6f2b608e3078700968599344619614
Fratsia <fauzantrisuladana@gmail.com>
2026-08-01 09:25:44 +0700

    feat(user seeder): seeder user ditambah aku pemuda

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,8 +20,16 @@ class UserSeeder extends Seeder
             'provider' => 'google',
             'id_provider' => '105102120350029888485',
             'profile_image' => 'https://lh3.googleusercontent.com/a/ACg8ocJgjcrmN_OSQiAu_cwa0iqqeCT2DJCNExzdL7ztc3_I2er2KRM1=s96-c',
-            'email_verified_at' => now(),
-            'password' => Hash::make('password'),
+            'activated_at' => now(),
+        ]);
+
+        User::create([
+            'name' => 'pemudanogotirto v',
+            'email' => 'pemudanogotirtov@gmail.com',
+            'status' => 'Aktif',
+            'provider' => 'google',
+            'id_provider' => '109612083245670967545',
+            'profile_image' => 'https://lh3.googleusercontent.com/a/ACg8ocLfPbny3Ji9mh5eCjtHmxKLPHIm-ozNYZt2NR1KZjnfb01e-Q=s96-c',
             'activated_at' => now(),
         ]);
     }
